6794f34df262305e26c50956ce76a3b2d0dcd7c8
Sarah Conway <sarahconway@MacBook-Pro-2.local>
2019-04-04 04:04:31 -0400

    mobile home functional

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-$(document).foundation()
+(() => {
+	
+console.log("yeet this yeast");
+
+	
+document.querySelector("#hamMenu").onclick = function() {showMenu()};
+document.querySelector("#backtotop").onclick = function() {toTop()};
+	
+	let menu = document.querySelector(".menu");
+	
+function showMenu(){ //this is how the hamburger menu shows up
+	menu.classList.toggle("show");
+}
+	
+function toTop(){ //this is the button that takes you back to the top on mobile
+	document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
+	
+
+
+})();
+
